551ea5ef9e84161a9d77452a30849043c7010db6
EdgardoSalazar <82413057+EdgardoSalazar@users.noreply.github.com>
2026-01-18 13:11:10 +0000

    Cambios models y routes

--- src/api/models.py
+++ src/api/models.py
@@ -1,21 +1,38 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 
 db = SQLAlchemy()
 
 
 class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = "users"
+    user_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), unique=False, nullable=False)
+    current_points = db.Column(db.Integer, default=0, nullable=False)
+    is_active = db.Column(db.Boolean(), default=True, nullable=False)
+    is_admin = db.Column(db.Boolean(), default=False, nullable=False)
+    registration_date = db.Column(db.DateTime, default=datetime.utcnow) 
+    trial_end_date = db.Column(db.DateTime, nullable=False)   
+    last_access = db.Column(db.DateTime, nullable=True)          
 
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
-        return {"id": self.id, 
-                "email": self.email,
-                'is_active': self.is_active}
+        return {
+            "user_id": self.user_id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "current_points": self.current_points,
+            "is_active": self.is_active,
+            "is_admin": self.is_admin,
+            "registration_date": self.registration_date,
+            "trial_end_date": self.trial_end_date, 
+            "last_access": self.last_access}
--- src/api/routes.py
+++ src/api/routes.py
@@ -11,8 +11,74 @@ api = Blueprint('api', __name__)
 CORS(api)  # Allow CORS requests to this API
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/users', methods=['GET', 'POST'])
+def users():
     response_body = {}
-    response_body['message'] = "Hello! I'm a message that came from the backend"
-    return response_body, 200
+
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Users)).scalars.all()
+        results = [row.serialize() for row in rows]
+        response_body['results'] = results
+        response_body['message'] = 'Listado de usuarios'
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json
+        row = Users(email=data.get('email'),
+                    password_hash=data.get('password'),
+                    first_name=data.get('first_name'),
+                    last_name=data.get('last_name'),
+                    is_active=True,
+                    is_admin=False,
+                    current_points=data.get('current_points'),
+                    registration_date=data.get('registration_date'),
+                    trial_end_date=data.get('trial_end_date'),
+                    last_access=data.get('last_acces'))
+        db.session.add(row)
+        db.session.commit()
+        response_body['results'] = row.serialize()
+        response_body['message'] = 'Usuario creado'
+        return response_body, 201
+    return response_body, 404
+
+
+@api.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def user(user_id):
+    response_body = {}
+
+    row = db.session.execute(
+        db.select(Users).where(Users.user_id == user_id)
+    ).scalar()
+
+    if not row:
+        response_body['message'] = 'Usuario no encontrado'
+        return response_body, 404
+
+    if request.method == 'GET':
+        response_body['results'] = row.serialize()
+        response_body['message'] = f'Detalles del usuario {user_id}'
+        return response_body, 200
+
+    if request.method == 'PUT':
+        data = request.json
+
+        row.email = data.get('email', row.email)
+        row.first_name = data.get('first_name', row.first_name)
+        row.last_name = data.get('last_name', row.last_name)
+        row.current_points = data.get('current_points', row.current_points)
+        row.is_active = data.get('is_active', row.is_active)
+        row.is_admin = data.get('is_admin', row.is_admin)
+        row.trial_end_date = data.get('trial_end_date', row.trial_end_date)
+        row.last_access = data.get('last_access', row.last_access)
+
+        db.session.commit()
+
+        response_body['results'] = row.serialize()
+        response_body['message'] = f'Usuario {user_id} actualizado'
+        return response_body, 200
+
+    if request.method == 'DELETE':
+        db.session.delete(row)
+        db.session.commit()
+
+        response_body['message'] = f'Usuario {user_id} eliminado'
+        return response_body, 200
